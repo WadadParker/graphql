73c0e08d0c786044e13f552d048311a0b8d23afe
Wadad Parker <wadadparker5@gmail.com>
2024-06-29 21:52:37 +0530

    feat: Codependent models
    How to access data present in one data from data present in other data. We define data types and call them that way

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,7 @@
 // We hardcoding data for now
 const data = {
     authors: [
-        {id:"1",name:"Author 1",bookIds:["101","102"]},
+        {id:"1",name:"Author 1",booksIds:["101","102"]},
         {id:"2",name:"Author 2",booksIds:["103"]},
     ],
     books: [
@@ -11,7 +11,26 @@ const data = {
     ]
 }
 // Well, simply return all authors from data. 
+// How to returned interdependent data? Like books having some author, we need to define author first in the resolver as a "data type"
+// then call it so that graphQl realizes this
 export const resolvers = {
+    // Over here, you need to define what Author means inside a book. And there are special arguments which is enforced by graphQl
+    // Remember that we are actually calling another schema so it should be case sensitive, author not Author
+    Book:{
+        author: (parent, args, context, info) => {
+            // Parent: Parent is going to have authorId, so we need to access parent to get relevant ids and info
+            
+            return data.authors.find(authorDetail=> authorDetail.id === parent.authorId)
+        }
+    },
+
+    Author: {
+        books:(parent,args,context,info) => {
+            console.log(parent)
+            return data.books.filter(book => parent.booksIds.includes(book.id))
+        }
+    },
+
     Query: {
         authors: () =>{
             return data.authors;
