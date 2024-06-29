d875e88295c4e1af9b49ded797b00b10c1ceb875
Wadad Parker <wadadparker5@gmail.com>
2024-06-29 21:34:03 +0530

    feat: returning simple array
    Structure the query in such a way that it returns you what you want. So instead of hardcoded data, you should return from the db

--- resolvers.js
+++ resolvers.js
@@ -1,17 +1,23 @@
+// We hardcoding data for now
+const data = {
+    authors: [
+        {id:"1",name:"Author 1",bookIds:["101","102"]},
+        {id:"2",name:"Author 2",booksIds:["103"]},
+    ],
+    books: [
+        {id:"101",title:"Book 1",publishedYear:2000,authorId:"1"},
+        {id:"102",title:"Book 2",publishedYear:2010,authorId:"1"},
+        {id:"103",title:"Book 3",publishedYear:2020,authorId:"2"}
+    ]
+}
+// Well, simply return all authors from data. 
 export const resolvers = {
     Query: {
         authors: () =>{
-            return [{
-                id: 1,
-                name:"Wadad Parker"
-            }]
+            return data.authors;
         },
         books: () => {
-            return [{
-                id:1,
-                title:"Namste frontend system design",
-                publishedYear: 2024
-            }]
+            return data.books;
         }
     }
 }
